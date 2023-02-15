ddc9b2dbd8c3616e73b7885b7d19566f79b6f0fa
en1993 <74547465+EN1993@users.noreply.github.com>
2023-02-15 19:55:59 +0100

    updated files

---
+++ eric/src/components/Services.js
@@ -0,0 +1,100 @@
+import dynamic from "next/dynamic";
+
+const Testimonials = dynamic(() => import("./Testimonials"), {
+  ssr: false,
+});
+
+const Services = () => {
+  return (
+    <section
+      id="services"
+      data-nav-tooltip="Services"
+      className="pp-section pp-scrollable section"
+    >
+      <div className="container">
+        <div className="title">
+          {/* <h3>What I do?</h3>  commented of 15.02.2023 */}
+          <h3>Hva gjøre jeg?</h3>
+        </div>
+        <div className="row">
+          <div className="col-sm-6 m-15px-tb">
+            <div className="feature-box-01 media">
+              <i className="icon theme-bg icon-mobile" />
+              <div className="feature-content media-body">
+                <h5>Web Design</h5>
+                <p>
+                  Jeg har udanning på hvordan det å designe nettside, 
+                  og ved å anvende riktige verktøy med fokus på bruker vennlig.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-6 m-15px-tb">
+            <div className="feature-box-01 media">
+              <i className="icon theme-bg icon-desktop" />
+              <div className="feature-content media-body">
+                {/* <h5>Web Development</h5>  commented of 15.02.2023 */}
+                <h5>Web Utvikling</h5>
+                <p>
+                  Kan gå god på å utvikle de flest nettsider 
+                  med god arkitektur resultat.
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* <div className="col-sm-6 m-15px-tb">
+            <div className="feature-box-01 media">
+              <i className="icon theme-bg icon-target" />
+              <div className="feature-content media-body">
+                <h5>SEO Marketing</h5>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+              </div>
+            </div>
+          </div>  commented of 15.02.2023 */}
+          {/* <div className="col-sm-6 m-15px-tb">
+            <div className="feature-box-01 media">
+              <i className="icon theme-bg icon-mobile" />
+              <div className="feature-content media-body">
+                <h5>Web Design</h5>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-6 m-15px-tb">
+            <div className="feature-box-01 media">
+              <i className="icon theme-bg icon-desktop" />
+              <div className="feature-content media-body">
+                <h5>Web Development</h5>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-6 m-15px-tb">
+            <div className="feature-box-01 media">
+              <i className="icon theme-bg icon-target" />
+              <div className="feature-content media-body">
+                <h5>SEO Marketing</h5>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+              </div>
+            </div>
+          </div>  commented of 15.02.2023 */}
+        </div>
+        {/* <div className="separated" />
+        <Testimonials />  commented of 15.02.2023 */}
+      </div>
+    </section>
+  );
+};
+export default Services;
