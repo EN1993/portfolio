9b0bd8ac2e4222d16b98841ad3afb7fc91528c5c
en1993 <74547465+EN1993@users.noreply.github.com>
2023-03-01 18:28:36 +0100

    updated about me image and typed animation

--- eric/.next/static/chunks/webpack.js
+++ eric/.next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ef225ab6f96a88df"; }
+/******/ 		__webpack_require__.h = function() { return "3644a2e74a218c5d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- eric/src/components/TypingAnimation.js
+++ eric/src/components/TypingAnimation.js
@@ -6,7 +6,7 @@ const TypingAnimation = () => {
 
   useEffect(() => {
     const typed = new Typed(el.current, {
-      strings: ["Web Designer", "Utvikler", "Freelancer"], // Strings to display
+      strings: ["Web Designer", "Web Utvikler", "Freelancer"], // Strings to display
       typeSpeed: 100,
       backSpeed: 100,
       backDelay: 100,
